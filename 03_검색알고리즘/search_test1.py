ee0c065f7b8519b9922ecaefe2ad3cb46229138e
Jiyoon Lim <jiyoonlim.x@gmail.com>
2024-06-26 11:38:55 +0900

    .

---
+++ 03_검색알고리즘/search_test1.py
@@ -0,0 +1,11 @@
+# seq_search() 함수를 사용하여 실수 검색하기
+from search_while import seq_search
+
+print('실수를 검색합니다')
+print('주의: END를 입력하면 종료합니다.')
+
+number = 0
+x = []
+
+while True:
+  
---
+++ 03_검색알고리즘/test1.py
@@ -0,0 +1,13 @@
+# for i in [[1,2], [10,20], [100,200]]:
+#   print(i)
+
+
+
+단어_사전 = {
+    '하나' : 'one',
+    '둘' : 'two',
+    '셋' : 'three',
+}
+
+for key,value in 단어_사전.items():
+  print(f'{key}: {value}')
