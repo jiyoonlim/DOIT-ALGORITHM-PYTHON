fcf66f9d3cf957331125e76555e61a1c25a94e08
Jiyoon Lim <jiyoonlim.x@gmail.com>
2024-06-27 11:43:03 +0900

    .

--- 08_리스트/linked_list_test.py
+++ 08_리스트/linked_list_test.py
@@ -5,7 +5,7 @@ from linked_list import LinkedList
 
 Menu = Enum('Menu', [ '머리에노드삽입', '꼬리에노드삽입', '머리노드삭제',
                      '꼬리노드삭제', '주목노드출력', '주목노드이동',
-                     '조목노드삭제', '모든노드삭제', '검색', '멤버십판단',
+                     '주목노드삭제', '모든노드삭제', '검색', '멤버십판단',
                      '모든노드출력', '스캔', '종료'])
 
 def select_Menu() -> Menu:
@@ -29,3 +29,29 @@ while True:
   elif menu == Menu.머리노드삭제: #맨 앞에 노드를 삭제
     lst.remove_first()
   elif menu == Menu.꼬리노드삭제: #맨 끝에 노드를 삭제
+    lst.remove_last()
+  elif menu == Menu.주목노드출력(): # 주목 노드를 출력
+    lst.print_current_node()
+  elif menu == Menu.주목노드이동(): #주목 노드를 한 칸 뒤로 이동
+    lst.next()
+  elif menu == Menu.주목노드삭제():
+    lst.remove_current_node()
+  elif menu == Menu.모든노드삭제: # 모든 노드를 삭제
+    lst.clear()
+  elif menu == Menu.검색: #노드를 검색
+    pos = lst.search(int(input('검색할 값을 입력하세요: ')))
+    if pos >= 0:
+      print(f'그 값의 데이터는 {pos + 1}번째에 있습니다')
+    else:
+      print('해당하는 데이터가 없습니다')
+  elif menu == Menu.멤버쉽판단: #멤버쉽을 판단
+    print('그 값은 데이터는 포함되어' +(' 있습니다' if int(input('판단할 값을 입력하세요: ')) in lst else ' 있지 않습니다.'))
+  elif menu == Menu.모든노드출력: # 모든 노드를 출력
+    lst.print()
+
+  elif menu == Menu.스캔: # 모든 노드를 스캔
+    for e in lst:
+      print(e)
+
+  else:
+    break
