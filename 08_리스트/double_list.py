8f17ffdbd2d53494a364ac86d588d6a9c421241c
Jiyoon Lim <jiyoonlim.x@gmail.com>
2024-06-28 08:26:27 +0900

    .

--- 08_리스트/double_list.py
+++ 08_리스트/double_list.py
@@ -28,3 +28,69 @@ class DoubleLinkedList:
   def is_empty(self) -> bool:
     """리스트가 비었느지 확인"""
     return self.head.next is self.head
+  
+  def search(self, data:Any) -> Any:
+    """data와 값이 같은 노드를 검색"""
+    cnt = 0
+    ptr = self.head.next # 현재 스캔 중인 노드
+    while ptr is not self.head:
+      if data == ptr.data:
+        self.current = ptr
+        return cnt # 검색 성공
+      cnt += 1
+      ptr = ptr.next # 뒤쪽 노드에 주목
+    return -1
+  
+  def __contains__(self, data:Any) -> bool:
+    """연결 리스트에 data가 포함되어 있는지 판단"""
+    return self.search(data) >= 0
+  
+  def print_current_node(self) -> None:
+    """주목 노드를 출력"""
+    if self.is_empty():
+      print('주목 노드는 없습니다.')
+    else:
+      print(self.current.data)
+
+  def print(self) -> None:
+    """모든 노드를 출력"""
+    ptr = self.head.next # 더미 노드의 뒤쪽 노드
+    while ptr is not self.head:
+      print(ptr.data)
+      ptr = ptr.next
+
+  def print_reverse(self) -> None:
+    """모든 노드를 역순으로 출력"""
+    ptr = self.head.prev # 더미 노드의 앞쪽 노드
+    while ptr is not self.head:
+      print(ptr.data)
+      ptr = ptr.prev
+
+  def next(self) -> bool:
+    """주목 노드를 한 칸 뒤로 이동"""
+    if self.is_empty() or self.current.next is self.head:
+      return False # 이동할 수 없음
+    self.current = self.current.next
+    return True
+  
+  def prev(self) -> bool:
+    """주목 노드를 한 칸 앞으로 이동"""
+    if self.is_empty() or self.current.prev is self.head:
+      return False #이동할 수 없음
+    self.current = self.current.prev
+    return True
+  
+  def add(self, data:Any) -> None:
+    """주목 노드 바로 뒤에 노드를 삽입"""
+    node = Node(data, self.current, self.current.next)
+    self.current.next.prev = node
+    self.current.next = node
+    self.current = node
+    self.no += 1
+
+  def add_first(self, data:Any) -> None:
+    """맨 앞에 노드를 삽입"""
+    self.current = self.head # 더미 노드 head의 바로 뒤에 삽입
+    self.add(data)
+
+  
