f2282291d95e016e567313760c0d3c7b44c6d90e
Jiyoon Lim <jiyoonlim.x@gmail.com>
2024-06-30 10:11:39 +0900

    .

--- 03_검색알고리즘/chained_hash.py
+++ 03_검색알고리즘/chained_hash.py
@@ -53,4 +53,27 @@ class ChainedHash:
   
   def remove(self, key:Any) -> bool:
     """키가 key인 원소를 삭제"""
-    hash = self.hash_value(key)
+    hash = self.hash_value(key) #삭제할 key의 해시값
+    p = self.table[hash] # 노드를 주목
+    pp = None # 바로 앞의 노드를 주목
+
+    while p is not None:
+      if p.key == key: # key를 발견하면 아래를 실행
+        if pp is None:
+          self.table[hash] = p.next
+        else:
+          pp.next = p.next
+        return True #key 삭제 성공
+      pp = p
+      p = p.next # 뒤쪽 노드를 주목
+    return False
+  
+  def dump(self) -> None:
+    """해시 테이블을 덤프"""
+    for i in range(self.capacity):
+      p = self.table[i]
+      print(i, end='')
+      while p is not None:
+        print(f' -> {p.key} ({p.value})', end='')
+        p = p.next
+      print()
