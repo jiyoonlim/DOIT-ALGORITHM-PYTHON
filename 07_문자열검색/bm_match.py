0559fb4dd96da8e50e56701f1b490861493dadaa
Jiyoon Lim <jiyoonlim.x@gmail.com>
2024-07-03 08:39:54 +0900

    .

--- 07_문자열검색/bm_match.py
+++ 07_문자열검색/bm_match.py
@@ -23,4 +23,12 @@ def bm_match(txt: str, pat: str) -> int:
   return -1
 
 if __name__ == '__main__':
-  
+  s1 = input('텍스트를 입력하세요: ')
+  s2 = input('패턴을 입력하세요: ')
+
+  idx = bm_match(s1, s2)
+
+  if idx == -1:
+    print('텍스트 안에 패턴이 존재하지 않습니다')
+  else:
+    print(f'{(idx + 1)} 번째 문자가 일치합니다')
