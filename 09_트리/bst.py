a473e94a74742192a4da7ab8d6f7b27647481af5
Jiyoon Lim <jiyoonlim.x@gmail.com>
2024-07-02 13:18:41 +0900

    .

--- 09_트리/bst.py
+++ 09_트리/bst.py
@@ -56,3 +56,102 @@ class BinarySearchTree:
       return True
     else:
       return add_node(self.root, key, value)
+    
+
+  def remove(self, key:Any) -> bool:
+    """키가 key인 노드를 삭제"""
+    p = self.root # 스캔 중인 노드
+    parent = None # 스캔 중인 노드의 부모 노드
+    is_left_child = True # p는 parent 왼쪽 자식 노드인지 확인
+
+    while True:
+      if p is None: #더 이상 진행할 수 없으면
+        return False # 그 키는 존재하지 않음
+      
+      if key == p.key: # key와 노드 p의 키가 같으면
+        break # 검색 성공
+      else:
+        parent = p # 가지를 내려가기 전에 부모를 설정
+        if key < p.key: # key 쪽이 작으면
+          is_left_child = True # 여기서 내려가는 것은 왼쪽 자식
+          p = p.left # 왼쪽 서브트리에서 검색
+        else:
+          is_left_child = False # 여기서 내려가는 것은 오른쪽 자식
+          p = p.right # 오른쪽 서브트리에서 검색
+    
+    if p.left is None: #p에 왼쪽 자식이 없으면
+      if p is self.root:
+        self.root = p.right
+      elif is_left_child:
+        parent.left = p.right #부모의 왼쪽 포인터가 오른쪽 자식을 가리킴
+      else:
+        parent.right = p.right # 부모의 오른쪽 포인터가 오른쪽 자식을 가리킴
+    elif p.right is None:
+      if p is self.root:
+        self.root = p.left
+      elif is_left_child:
+        parent.left = p.left # 부모의 왼쪽 포인터가 왼쪽 자식을 가리킴
+      else:
+        parent.right = p.left # 부모의 오른쪽 포인터가 왼쪽 자식을 가리킴
+    else:
+      parent = p
+      left = p.left # 서브트리 안에서 가장 큰 노드
+      is_left_child = True
+      while left.right is not None: # 가장 큰 노드 left를 검색
+        parent = left
+        left = left.right
+        is_left_child = False
+      
+      p.key = left.key # left 의 키를 p로 이동
+      p.value = left.value
+      if is_left_child:
+        parent.left = left.left # left를 삭제
+      else:
+        parent.right = left.left # left를 삭제
+    return True
+  
+  def dump(self) -> None:
+    """덤프(모든 노드를 키의 오름차순으로 출력)"""
+    def print_subtree(node: Node):
+      """node를 루트로 하는 서브트리의 노드를 키의 오름차순으로 출력"""
+      if node is not None:
+        print_subtree(node.left) # 왼쪽 서브트리를 오름차순으로 출력
+        print(f'{node.key} {node.value}') # 노드를 출력
+        print_subtree(node.right) # 오른쪽 서브트리를 오름차순으로 출력
+    print_subtree(self.root)
+
+  def min_key(self) -> Any:
+    """가장 작은 키"""
+    if self.root is None:
+      return None
+    p = self.root
+    while p.left is not None:
+      p = p.left
+    return p.key
+  
+  def max_key(self) -> Any:
+    """가장 큰 키"""
+    if self.root is None:
+      return None
+    p = self.root
+    while p.right is not None:
+      p = p.right
+    return p.key
+  
+  def dump2(self, reverse=False) -> None:
+    """덤프(모든 노드를 키의 오름차순/내림차순으로 출력)"""
+    def print_subtree(node: Node):
+      """node를 루트로 하는 서브트리의 노드를 키의 오름차순으로 출력"""
+      if node is not None:
+        print_subtree(node.left) # 왼쪽 서브트리를 오름차순으로 출력
+        print(f'{node.key} {node.value}') # nodefmf cnffur
+        print_subtree(node.right) # 오른쪽 서브트리를 오름차순으로 출력
+
+    def print_subtree(node:Node):
+      """node를 루트로 하는 서브트리의 노드를 키의 내림차순으로 출력"""
+      if node is not None:
+        print_subtree_rev(node.right) # 오른쪽 서브트리를 내림차순으로 출력
+        print(f'{node.key} {node.value}') # 노드를 출력
+        print_subtree_rev(node.left) # 왼쪽 서브트리를 내림차순으로 출력
+    
+    print_subtree_rev(self.root) if reverse else print_subtree(self.root)
---
+++ 09_트리/bst_test.py
@@ -0,0 +1,5 @@
+# 이진 검색 트리 클래스 BinarySearchTree 사용하기
+
+from enum import Enum
+from bst import BinarySearchTree
+
