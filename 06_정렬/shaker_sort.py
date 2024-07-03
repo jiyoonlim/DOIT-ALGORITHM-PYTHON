16b7a9739c3be20858bbc9e1ee68f6f0c8c53f32
Jiyoon Lim <jiyoonlim.x@gmail.com>
2024-07-03 09:31:51 +0900

    .

--- 06_정렬/shaker_sort.py
+++ 06_정렬/shaker_sort.py
@@ -10,4 +10,14 @@ def shaker_sort(a: MutableSequence) -> None:
   while left < right:
     for j in range(right, left, -1):
       if a[j-1] > a[j]:
-        a[j-1], 
+        a[j-1], a[j] = a[j], a[j-1]
+        last = j
+    left = last
+
+    for j in range(left, right):
+      if a[j] > a[j+1]:
+        a[j], a[j+1] = a[j+1], a[j]
+        last = j
+    right = last
+
+    
